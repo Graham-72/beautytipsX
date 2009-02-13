4a0bae87f62a1dfbb0bf6557c98194b79678d303
kleinmp <kleinmp@390447.no-reply.drupal.org>
2009-02-13 22:50:40 +0000

    Got rid of theme_textfield override.

--- beautytips.js
+++ beautytips.js
@@ -1,8 +1,16 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-  	$('form .form-item input.form-text + .description').hide().prev('input').bt({
-	    trigger: ['focus', 'blur'],
-	    positions: [bt_direction]
-	  });
+    var text = ['textarea.form-textarea', '.form-text'];
+    for (var i = 0; i < text.length; i++) {
+      $(text[i]).each(function() {
+        var description = $(this).parents('.form-item').find('.description').hide().html();
+        if (description) {
+          $(this).bt(description, {
+            trigger: ['focus', 'blur'],
+  	        positions: [bt_direction]
+          });
+        }
+      });
+    }
   });
 }
