c31ae578e9b67349606fcfb2c5dd3bce1a8a8c49
matt <matt@zivtech.com>
2012-05-04 15:13:49 -0400

    Moving styles in javascript so that they're separate from the tolltips themselves

--- js/beautytips.js
+++ js/beautytips.js
@@ -1,7 +1,7 @@
 
 /**
  * Defines the default beautytip and adds them to the content on the page
- */ 
+ */
 (function ($) {
   Drupal.behaviors.beautytips = {
     attach: function(context, settings) {
@@ -11,38 +11,12 @@
       }
       jQuery.bt.options.closeWhenOthersOpen = true;
       var beautytips = Drupal.settings.beautytips;
+      var styles = Drupal.settings.beautytipStyles;
 
-      // On ajax page loads, if the same settings are added
-      // to the page, then it can mess up the settings.
-      // If this is fixed in Drupal, then this can be removed.
-      function fixArray(originalArray, count) {
-        for (var key in originalArray) {
-          if (key == 'cssStyles') {
-            originalArray[key] = fixArray(originalArray[key], count);
-          }
-          else if (originalArray[key].length == count) {
-            originalArray[key] = originalArray[key][0];
-          }
-          else {
-            length = Math.round(originalArray[key].length / count);
-            originalArray[key] = originalArray[key].slice(0, length);
-          }
-        }
-        return originalArray;
-      }
-
-      // Add the the tooltips to the page
+      // Add the tooltips to the page
       for (var key in beautytips) {
-        // If there's an ajax page load on a page, drupal can add these
-        // settings more than once and it adds the settings instead of replaces.
-        // We have to fix these here.
-        if (typeof(Drupal.settings.beautytips[key]['cssSelect']) == 'object') {
-          var count = Drupal.settings.beautytips[key]['cssSelect'].length;
-          beautytips[key] = fixArray(beautytips[key], count);
-          Drupal.settings.beautytips[key] = beautytips[key];
-        }
         // Build array of options that were passed to beautytips_add_beautyips
-        var btOptions = new Array();
+        var btOptions = styles[beautytips[key]['style']];
         if (beautytips[key]['list']) {
           for ( var k = 0; k < beautytips[key]['list'].length; k++) {
             btOptions[beautytips[key]['list'][k]] = beautytips[key][beautytips[key]['list'][k]];
